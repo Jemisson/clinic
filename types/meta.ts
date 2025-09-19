fd07644829327fbb1bf121034047289ed7f52aa6
murilodestefani <murilo_destefani@hotmail.com>
2025-09-19 12:50:20 -0400

    Add user management features with data table and filtering options

---
+++ types/meta.ts
@@ -0,0 +1,7 @@
+export interface Meta {
+  total_count: number;
+  total_pages: number;
+  current_page: number;
+  per_page: number;
+}
+
---
+++ types/users.ts
@@ -0,0 +1,40 @@
+import { Meta } from "./meta";
+import { TagData } from "./tags";
+
+export interface UserResponse {
+  data: ProfileUserData[];
+  meta: Meta;
+}
+
+export interface ProfileUserData {
+  id: string;
+  type: "profile_user";
+  attributes: ProfileUserAttributes;
+}
+
+export interface ProfileUserAttributes {
+  name: string;
+  cpf: string;
+  rg: string;
+  birthdate: string;
+  address: string;
+  mobile_phone: string;
+  sector: string;
+  job_function: string;
+  profile_children: ProfileChild[];
+  role: string;
+  email: string;
+  tags: TagData[];
+  photo_url: string | null;
+  photo_thumb_url: string | null;
+}
+
+export interface ProfileChild {
+  id: number;
+  profile_user_id: number;
+  name: string;
+  degree: string;
+  birth: string;
+  created_at: string;
+  updated_at: string;
+}
