a9a30f1d4755c6faff12ecda5a3932d5c48ce377
Jemisson <jemison321@gmail.com>
2025-11-25 17:03:00 -0400

    Adiciona Event-Calendar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { Toaster } from 'sonner'
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -34,7 +37,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <LayoutWrapper>{children}</LayoutWrapper>
+        <NuqsAdapter>
+          <LayoutWrapper>{children}</LayoutWrapper>
+          <Toaster
+            richColors
+            position="top-center"
+          />
+        </NuqsAdapter>
       </body>
     </html>
   )
